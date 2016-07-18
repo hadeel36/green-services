1f198d4c2301295ebd52d407e249353e4ad0981f
housamaldeen <housam993@gmail.com>
2016-07-18 12:31:54 +0300

    Add services function

--- client/app/Users/Users.js
+++ client/app/Users/Users.js
@@ -3,9 +3,16 @@ angular.module('GS.Users', [])
 .controller('UserController', function ($scope, Orders) {
   // Your code here
   ///this function must call on submit click
-  $scope.addOrder=function(order){
-  	console.log('hey')
-  	Orders.addOneOrder(order).then(function(){
+  $scope.order = {};
+  $scope.order.serviceType;
+        $scope.getVal=function(event){
+            $scope.order.serviceType=event.currentTarget.value;
+        }
+  $scope.order.fullName = 'samer';
+  $scope.order.Address = 'irbid';
+  $scope.addOrder=function(){
+  	console.log($scope.order)
+  	Orders.addOneOrder($scope.order).then(function(){
   	})
   	.catch(function(err){
   		console.log(err);
--- client/app/serviceProvider/serviceprovider.js
+++ client/app/serviceProvider/serviceprovider.js
@@ -1,13 +1,16 @@
 angular.module('GS.serviceProvider', [])
 
 .controller('serviceproviderController', function ($scope, Services) {
-  // Your code here
   $scope.data={};
+  $scope.data.orders=[{order_id:"5",fullname:"eshraq",phoneNumber:"0799999",address:"amman",quentity:"2"},{order_id:"4",fullname:"hussam",phoneNumber:"079ddd9",address:"irbid",quentity:"3"}];
   	Services.getAllOrders().then(function(data){
-  		$scope.data.links=data;
+  		
   	})
   	.catch(function(err){
   		console.error(err);
   	});
-  
+  $scope.delete=function(index){
+  	console.log(index);
+  	Services.delivered($scope.data.orders[index].order_id);//order_id depend on database
+  }
 });
